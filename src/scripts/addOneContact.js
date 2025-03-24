c3433d3d2dca04f172338feadc7d1eac5a735fc5
Olexandra Ghordievska <maynarisochka@gmail.com>
2025-03-24 14:01:45 +0200

    update code

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,12 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+  const contacts = await readContacts();
+  const updatedContacts = [...contacts, createFakeContact()];
+  await writeContacts(updatedContacts);
+  console.log('One contact added.');
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,15 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const generateContacts = async (number = 5) => {
+  const contacts = await readContacts();
+  const updatedContacts = [
+    ...contacts,
+    ...Array(number).fill(0).map(createFakeContact),
+  ];
+  await writeContacts(updatedContacts);
+  console.log(`Added ${number} new contacts.`);
+};
 
 generateContacts(5);
